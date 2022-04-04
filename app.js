af4b4741569774ce70b2dfd9bf3aa538a4467181
Daniel Zandi <daniel.zandi@egt-interactive.com>
2022-04-04 21:34:20 +0300

    safe commit

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// app.get('/', (req,res) => {
-// 	res.send('Hello world!');
-// });
+app.get('/', (req,res) => {
+	res.send('Hello world!');
+});
 
 app.get('/demo', (req,res) => {
 	res.set('X-Alien-Power', 'Mothership');
