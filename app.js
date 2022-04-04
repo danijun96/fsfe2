3a8c734455ff7ce35026b4be5c7b859513a234e4
Daniel Zandi <daniel.zandi@egt-interactive.com>
2022-04-04 21:29:38 +0300

    test

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
-	res.send('Hello world!');
-});
+// app.get('/', (req,res) => {
+// 	res.send('Hello world!');
+// });
+
 app.get('/demo', (req,res) => {
+	res.set('X-Alien-Power', 'Mothership');
 	res.status(418);
-	res.set('X-Alien-Power', 'Mothership')
 	res.send('Hello Earthlings!');
 });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
