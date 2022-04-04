0348364342a5322d6f1ac866fb6320d097042bab
Daniel Zandi <daniel.zandi@egt-interactive.com>
2022-04-04 21:08:10 +0300

    new route demo added

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const port = 3000;
 app.get('/', (req,res) => {
 	res.send('Hello world!');
 });
-
+app.get('/demo', (req,res) => {
+	res.status(418);
+	res.set('X-Alien-Power', 'Mothership')
+	res.send('Hello Earthlings!');
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
